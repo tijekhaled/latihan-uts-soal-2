00c4ef4f40a9004933c5c6536fda77945b2cc560
tijekhaled <116840456+tijekhaled@users.noreply.github.com>
2022-10-28 22:58:11 +0700

    Add files via upload

---
+++ soal-2.py
@@ -0,0 +1,19 @@
+# Latihan Soal UTS KU1102 K16
+# Asisten Akademik: Theodore Maximillan Jonathan (13219021)
+
+# Nomor Soal    :
+# NIM           : 
+# Nama          :
+
+# PROGRAM _______________ [isi dengan judul]
+# Tuliskan judul program dan penjelasan singkat.
+
+# KAMUS
+# Isilah dengan definisi variabel yang digunakan.
+
+# ALGORITMA
+# Jangan lupa memberikan komentar secukupnya.
+
+# Output harus disimpan dalam matriks yang baru, bernama result.
+# Sintaks di bawah harus dimasukkan dalam program untuk mencetak output.
+print(result)
